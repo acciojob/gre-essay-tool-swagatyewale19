08d48500a8817ced8b89aeb60ad284154cd43147
Swagat Yewale <120251460+swagatyewale19@users.noreply.github.com>
2023-02-26 23:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+function countWords() {
+			// Get the text from the textarea
+			const text = document.getElementById("evaluatedText").value;
+
+			// Count the number of words
+			const wordCount = text.trim().split(/\s+/).length;
+
+			// Update the word count in the h3 tag
+			document.getElementById("wordCount").textContent = wordCount;
+		}
